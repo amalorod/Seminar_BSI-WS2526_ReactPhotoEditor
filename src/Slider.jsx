b59208790532457179fac52f829c5fdd34933d5b
amalorod <alicmalorodow@gmail.com>
2025-12-28 14:58:49 +0100

    Add files via upload

---
+++ src/Slider.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+
+export default function Slider({min, max, value, handleChange}) {
+ 
+    return (
+      <div className="slider-container">
+        <input 
+        type="range" 
+        className='slider'
+        min={min}
+        max={max}
+        value={value}
+        onChange = {handleChange}>
+
+
+
+
+        </input>
+
+
+
+      </div>
+    )
+  }
+
+
+
